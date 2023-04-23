fdfa12c8d38ef6036916c964c477f8c0b5379df5
Mikhail <mikhail86@MyLin.myguest.virtualbox.org>
2023-04-23 13:08:40 +0400

    Add FilmFull model.

--- src/models/models.py
+++ src/models/models.py
@@ -1,8 +1,11 @@
+from pydantic import Field
+
 from mixins import ORJSONMixin, TimestampedMixin, UUIDMixin
 
 
-class Film(UUIDMixin, TimestampedMixin, ORJSONMixin):
+class FilmShort(UUIDMixin, TimestampedMixin, ORJSONMixin):
     title: str
+    imdb_rating: float
     description: str
 
 
@@ -13,3 +16,11 @@ class Genre(UUIDMixin, TimestampedMixin, ORJSONMixin):
 
 class Person(UUIDMixin, TimestampedMixin, ORJSONMixin):
     full_name: str
+
+
+class FilmFull(FilmShort):
+    genre: list[str] | None = Field(default=[])
+    actors: list[Person] | None = Field(default=[])
+    writers: list[Person] | None = Field(default=[])
+    director: list[str] | None = Field(default=[])
+
