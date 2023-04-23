2ed665fbacfe69920b295ae19129b862fd5eb3db
Arthur Alty <artouralty.jesus@gmail.com>
2023-04-24 02:03:12 +0300

    postgres data extractor created

--- etl/main.py
+++ etl/main.py
@@ -1,9 +1,15 @@
 from elasticsearch import Elasticsearch
+from utils.index_settings import create_es_index
 
 
 es = Elasticsearch(hosts='http://elastic_search:9200/')
 
+BLOCK_SIZE = 500
+
+INDEX_NAME = 'person_index'
 
 
 if __name__ == '__main__':
-    pass
+    
+    if not es.indices.exists(index=INDEX_NAME):
+        create_es_index(es, INDEX_NAME)
---
+++ etl/pg_extractor.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+
+import pydantic
+from psycopg2.extensions import connection as pg_connection
+
+from utils.models import PersonInfo
+
+
+class PostgresExtractor:
+
+    def __init__(self, connection: pg_connection, block_size: int) -> None:
+        self.connection = connection
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('SET search_path TO content;')
+        self.cursor.arraysize = block_size
+    
+    def extract_data(
+            self, start_time: datetime,
+            extract_time: datetime, excluded_ids: list
+    ) -> list:
+        """Extracting person data from PostgreSQL."""
+
+        query = f"""
+            SELECT p.id, p.full_name
+            FROM persons p
+            WHERE p.modified > '{extract_time}'
+        """
+
+        # if excluded_ids:
+        #     query += f"""
+        #         AND (
+        #             p.id not in '{excluded_ids}' OR
+        #             MAX(p.modified) > '{start_time}'
+        #         )
+        #     """
+        
+        query += 'ORDER BY id DESC;'
+
+        self.cursor.execute(query)
+
+        while True:
+            data = self.cursor.fetchmany()
+
+            if not data:
+                break
+
+            model_data = []
+
+            for row in data:
+                try:
+                    model_data.append(PersonInfo(**row))
+                except pydantic.ValidationError as exc:
+                    pass # logging in future
+            
+                # excluded_ids.append(row['id'])
+            
+            yield model_data
+        
+        self.cursor.close()
