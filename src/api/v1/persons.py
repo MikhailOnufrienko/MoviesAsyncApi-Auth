e4c2c73462a7b5bcd85b0fc2a440d262c55ed398
Arthur Alty <artouralty.jesus@gmail.com>
2023-05-03 20:52:37 +0300

    persons api endpoints are done

--- src/api/v1/persons.py
+++ src/api/v1/persons.py
@@ -10,15 +10,29 @@ from services.person import PersonService, get_person_service
 router = APIRouter()
 
 
-class PersonShortFilm(BaseModel):
-    uuid: str
-    roles: list
+class FilmPersonRoles(BaseModel):
+    id: str
+    roles: list[str]
+
+
+class PersonShortFilmInfo(BaseModel):
+    id: str
+    title: str
+    imdb_rating: float
+
+
+class PersonShortFilmInfoList(BaseModel):
+    results: list[PersonShortFilmInfo]
 
 
 class Person(BaseModel):
     id: str
     full_name: str
-    films: list[PersonShortFilm]
+    films: list[FilmPersonRoles]
+
+
+class PersonList(BaseModel):
+    results: list[Person]
 
 
 @router.get('/search')
@@ -27,23 +41,20 @@ async def person_list_search(
     page: int = 1,
     page_size: int = 10,
     query: str | None = None
-) -> list:
+) -> list[Person]:
     """API Endpoint for a list of persons and their roles in films."""
 
-    objects = await person_service.get_object_list(page, page_size, query)
-    data = []
+    objects = await person_service.get_person_list(page, page_size, query)
 
-    for item in objects:
-        person, films = item
-        data.append(
+    return PersonList(
+        results=[
             Person(
                 id=person.id,
                 full_name=person.full_name,
-                films=films
-            )
-        )
-
-    return data
+                films=person.films
+            ) for person in objects
+        ]
+    )
 
 
 @router.get('/{person_id}', response_model=Person)
@@ -53,26 +64,38 @@ async def person_detail(
 ) -> Person | HTTPException:
     """API Endpoint to retrieve information about a person by his id."""
 
-    person, movies = await person_service.get_by_id(person_id)
+    person = await person_service.get_by_id(person_id)
 
     if not person:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='person not found'
         )
 
-    return Person(id=person.id, full_name=person.full_name, films=movies)
+    return Person(
+        id=person.id,
+        full_name=person.full_name,
+        films=person.films,
+    )
 
 
 @router.get('/{person_id}/film')
-async def person_detail_films(
+async def person_films_detail(
     person_id: str,
     person_service: PersonService = Depends(get_person_service)
-) -> list:
+) -> list[FilmPersonRoles]:
     """
     API Endpoint to get information about films
     in which the person was involved.
     """
 
-    movies = await person_service.get_films_by_person(person_id)
+    films = await person_service.get_person_films_list(person_id)
 
-    return movies
+    return PersonShortFilmInfoList(
+        results=[
+            PersonShortFilmInfo(
+                id=film.id,
+                title=film.title,
+                imdb_rating=film.imdb_rating,
+            ) for film in films
+        ]
+    )
