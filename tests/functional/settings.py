a3680f9c7ca8db65f0d2e2e50c450ed8a9cadbc6
Mikhail <mikhail86@MyLin.myguest.virtualbox.org>
2023-05-14 16:24:53 +0400

    Add connection settings to test genres.

---
+++ tests/functional/settings.py
@@ -0,0 +1,14 @@
+from pydantic import BaseSettings, Field
+
+
+class TestSettings(BaseSettings):
+    es_host: str = Field('http://127.0.0.1:9200', env='ELASTIC_HOST')
+    es_genre_index: str = 'genres'
+    es_id_field: str = 'id'
+    es_index_mapping: dict = {}
+
+    redis_host: str = Field('http://127.0.0.1:6379', env='REDIS_HOST')
+    service_url: str = Field('http://127.0.0.1:8000/api/v1/', env='SERVICE_URL')
+ 
+
+test_settings = TestSettings()
