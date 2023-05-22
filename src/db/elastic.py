33caad36b90ea33e76d26cb0edc25279521db2c8
Mikhail <mikhail86@MyLin.myguest.virtualbox.org>
2023-05-22 23:56:26 +0400

    Remove commented lines in db/elastic.py and db/redis.py.

--- src/db/elastic.py
+++ src/db/elastic.py
@@ -4,24 +4,10 @@ from elasticsearch import AsyncElasticsearch
 
 
 class AsyncSearchAbstract(ABC):
- #   @abstractmethod
-  #  async def get_search_connection(self):
-        pass
-
-    
-#class AsyncSearchImplementation(AsyncSearchInterface):
-#    def __init__(self, elastic):
-#        self.elastic = elastic
+    pass
 
 
 elastic: AsyncElasticsearch = AsyncElasticsearch(hosts='http://localhost:9200/')
 
 async def get_elastic() -> AsyncElasticsearch:
     return elastic
-
-#async def main():
-#    search_implementation = AsyncSearchImplementation()
-#    es = search_implementation.get_search_connection()
-#    return es
-
-#es = AsyncSearchImplementation()
--- src/db/redis.py
+++ src/db/redis.py
@@ -4,28 +4,10 @@ from redis.asyncio import Redis
 
 
 class AsyncCacheAbstract(ABC):
-#    @abstractmethod
-#    async def get_cache_connection(self):
-        pass
-
-
-#class AsyncCacheImplementation(AsyncCacheInterface):
-#    def __init__(self, redis):
-#        self.redis = redis
-
-#    async def get_cache_connection(self):
-#        return Redis()
+    pass
 
 
 redis: Redis = Redis()
 
 async def get_redis() -> Redis:
     return redis
-
-
-#async def main():
-#    cache_implementation = AsyncCacheImplementation()
-#    conn = await cache_implementation.get_cache_connection()
-#    return conn
-
-#redis = AsyncCacheImplementation()
