c9a640bd0cf43cb010af468bf27663e3073ff39f
Arthur Alty <artouralty.jesus@gmail.com>
2023-05-27 00:10:39 +0300

    minor pep8 fixes and variable names changes

--- src/services/film.py
+++ src/services/film.py
@@ -11,19 +11,21 @@ from db.redis import AsyncCacheAbstract, get_redis, redis
 from models.models import FilmFull, FilmShort
 from redis.asyncio import Redis
 
+
 FILM_CACHE_EXPIRE_IN_SECONDS = settings.REDIS_CACHE_EXPIRES_IN_SECONDS
 INDEX_NAME = settings.ES_MOVIE_INDEX
 
 
 class ElasticService(AsyncSearchAbstract):
+    """Class to represent search engine with ElasticSearch."""
+
     def __init__(self, elastic: AsyncElasticsearch, index_name: str):
         self.elastic = elastic
         self.index_name = index_name
 
     async def _get_single_object(self, film_id: str) -> FilmFull | None:
-        """Retrieve a film instance from Elasticsearch DB.
+        """Retrieve a film instance from Elasticsearch DB."""
 
-        """
         try:
             doc = await self.elastic.get(index=self.index_name, id=film_id)
         except NotFoundError:
@@ -34,9 +36,7 @@ class ElasticService(AsyncSearchAbstract):
         self,
         search_query: dict
     ) -> tuple[int, list[FilmShort]]:
-        """Return a list of movies from Elasticsearch DB with a paginator.
-
-        """
+        """Return a list of movies from Elasticsearch DB with a paginator."""
 
         result = await self.elastic.search(
             index=self.index_name,
@@ -47,6 +47,7 @@ class ElasticService(AsyncSearchAbstract):
 
         if not hits:
             return total, []
+
         try:
             films = [hits[i]['_source'] for i in range(search_query['size'])]
         except IndexError:
@@ -56,26 +57,31 @@ class ElasticService(AsyncSearchAbstract):
 
 
 class RedisService(AsyncCacheAbstract):
+    """Class to represent cache service with Redis."""
+
     def __init__(self, redis: Redis):
         self.redis = redis
 
     async def _get_single_object(self, film_id: str) -> FilmFull | None:
-        """Retrieve a film instance from Redis cache.
+        """Retrieve a film instance from Redis cache. """
 
-        """
         cache_key = f'film:{film_id}'
         data = await self.redis.get(cache_key)
+
         if not data:
             return None
+
         return FilmFull.parse_raw(data)
 
     async def _get_list_of_objects(
-        self, page: int, size: int,
-        query: str = None, genre: UUID = None
+        self,
+        page: int,
+        size: int,
+        query: str = None,
+        genre: UUID = None
     ) -> tuple[int, list[FilmShort]]:
-        """Retrieve films from Redis cache.
+        """Retrieve films from Redis cache. """
 
-        """
         cache_key = f'films:{page}:{size}:{query}:{genre}'
         data = await self.redis.get(cache_key)
 
@@ -89,9 +95,8 @@ class RedisService(AsyncCacheAbstract):
         return total, films
 
     async def _put_single_object(self, film: FilmFull):
-        """Save a film instance to Redis cache.
+        """Save a film instance to Redis cache."""
 
-        """
         cache_key = f'film:{str(film.id)}'
 
         await self.redis.set(
@@ -108,63 +113,60 @@ class RedisService(AsyncCacheAbstract):
         films: list[FilmShort],
         query: str = None,
         genre: UUID = None
-    ):
-        """Save films to Redis cache.
+    ) -> None:
+        """Save films to Redis cache."""
 
-        """
         cache_key = f'films:{page}:{size}:{query}:{genre}'
         data = {
             'total': total,
             'films': [film.json() for film in films]
         }
         json_str = json.dumps(data)
-        await self.redis.set(cache_key, json_str,
-                             FILM_CACHE_EXPIRE_IN_SECONDS)
+
+        await self.redis.set(
+            cache_key, json_str, FILM_CACHE_EXPIRE_IN_SECONDS
+        )
 
 
-redis_service = RedisService(redis)
-es_service = ElasticService(elastic, INDEX_NAME)
+cache_service = RedisService(redis)
+search_service = ElasticService(elastic, INDEX_NAME)
 
 
 class FilmService:
     """Class to represent films logic."""
 
     def __init__(
-            self,
-            rs: AsyncCacheAbstract,
-            es: AsyncSearchAbstract,
-            index_name: str
-    ):
-        self.redis_service = rs
-        self.elastic_service = es
+        self,
+        redis: AsyncCacheAbstract,
+        elastic_search: AsyncSearchAbstract,
+        index_name: str
+    ) -> None:
+        self.redis_service = redis
+        self.es_service = elastic_search
         self.index_name = index_name
 
     async def get_films(
-            self,
-            page: int,
-            size: int,
-            genre: UUID
+        self,
+        page: int,
+        size: int,
+        genre: UUID
     ) -> tuple[int, list[FilmShort]]:
-        """Retrieve films instances to list films
-        in accordance with filtration conditions.
-
         """
-        total, films = await redis_service._get_list_of_objects(
+        Retrieve films instances to list films
+        in accordance with filtration conditions.
+        """
+
+        total, films = await cache_service._get_list_of_objects(
             page, size, genre
         )
 
         if not films:
             start_index = (page - 1) * size
+
             if not genre:
                 search_query = {
-                    "query": {
-                        "match_all": {}
-                    },
-                    "sort": [
-                        {
-                            "imdb_rating": {"order": "desc"}
-                        }
-                    ],
+                    "query": {"match_all": {}},
+                    "sort": [{"imdb_rating": {"order": "desc"}}],
                     "from": start_index,
                     "size": size
                 }
@@ -176,27 +178,32 @@ class FilmService:
                             "query": {
                                 "bool": {
                                     "filter": [
-                                        {"term": {"genres.id": genre}}
+                                        {
+                                            "term": {"genres.id": genre}
+                                        }
                                     ]
                                 }
                             }
                         }
                     },
-                    "sort": [
-                        {
-                            "imdb_rating": {"order": "desc"}
-                        }
-                    ],
+                    "sort": [{"imdb_rating": {"order": "desc"}}],
                     "from": start_index,
                     "size": size
                 }
 
-            total, films = await es_service._get_list_of_objects(search_query)
+            total, films = await search_service._get_list_of_objects(
+                search_query
+            )
+
             if not films:
                 return 0, None
-            await redis_service._put_list_of_objects(page, size,
-                                                     total, films, genre)
+
+            await cache_service._put_list_of_objects(
+                page, size, total, films, genre
+            )
+
             return total, films
+
         return total, films
 
     async def search_films(
@@ -205,53 +212,55 @@ class FilmService:
         size: int,
         query: str
     ) -> tuple[int, list[FilmShort]]:
-        """Retrieve films instances to list films
+        """
+        Retrieve films instances to list films
         in accordance with search conditions.
-
         """
 
-        total, films = await redis_service._get_list_of_objects(page,
-                                                                size, query)
+        total, films = await cache_service._get_list_of_objects(
+            page, size, query
+        )
 
         if not films:
             start_index = (page - 1) * size
             search_query = {
-                "query": {
-                    "match": {
-                        "title": query
-                    }
-                },
+                "query": {"match": {"title": query}},
                 "sort": [
-                    {
-                        "_score": {"order": "desc"}
-                    },
-                    {
-                        "imdb_rating": {"order": "desc"}
-                    }
+                    {"_score": {"order": "desc"}},
+                    {"imdb_rating": {"order": "desc"}},
                 ],
                 "from": start_index,
                 "size": size
             }
 
-            total, films = await es_service._get_list_of_objects(search_query)
+            total, films = await search_service._get_list_of_objects(
+                search_query
+            )
+
             if not films:
                 return 0, None
-            await redis_service._put_list_of_objects(page, size,
-                                                     total, films, query)
+
+            await cache_service._put_list_of_objects(
+                page, size, total, films, query
+            )
+
             return total, films
+
         return total, films
 
     async def get_by_id(self, film_id: str) -> FilmFull | None:
-        """Return a film instance in accordance with ID given.
+        """Return a film instance in accordance with ID given."""
 
-        """
-        film = await redis_service._get_single_object(film_id)
+        film = await cache_service._get_single_object(film_id)
 
         if not film:
-            film = await es_service._get_single_object(film_id)
+            film = await search_service._get_single_object(film_id)
+
             if not film:
                 return None
-            await redis_service._put_single_object(film)
+
+            await cache_service._put_single_object(film)
+
         return film
 
 
--- src/services/person.py
+++ src/services/person.py
@@ -18,13 +18,13 @@ INDEX_NAME = settings.ES_PERSON_INDEX
 
 
 class ElasticService(AsyncSearchAbstract):
+    """Class to represent search engine with ElasticSearch."""
+
     def __init__(self, elastic: AsyncElasticsearch, index_name: str):
         self.elastic = elastic
         self.index_name = index_name
 
-    async def _get_single_object(
-        self, person_id: str
-    ) -> PersonFull | None:
+    async def _get_single_object(self, person_id: str) -> PersonFull | None:
         """Request to ElasticSearch to get person data."""
 
         try:
@@ -48,10 +48,15 @@ class ElasticService(AsyncSearchAbstract):
         )
 
     async def _get_list_of_objects(
-        self, query, page_size, from_page
+        self,
+        query: str,
+        page_size: int,
+        from_page: int
     ) -> tuple[int, list[PersonFull]]:
-        """Request to ElasticSearch to get a list of persons found
-           in accordance with the query."""
+        """
+        Request to ElasticSearch to get a list of persons found
+        in accordance with the query.
+        """
 
         response = await self.elastic.search(
             index=self.index_name, query=query, from_=from_page,
@@ -59,9 +64,12 @@ class ElasticService(AsyncSearchAbstract):
         )
         total = response['hits']['total']['value']
         results = response['hits']['hits']
+
         if not results:
             return total, []
+
         data = []
+
         for item in results:
             person = item['_source']
             _, films = await get_films(self.elastic, person['full_name'])
@@ -78,10 +86,13 @@ class ElasticService(AsyncSearchAbstract):
                     ]
                 )
             )
+
         return total, data
 
 
 class RedisService(AsyncCacheAbstract):
+    """Class to represent cache service with Redis."""
+
     def __init__(self, redis: Redis):
         self.redis = redis
 
@@ -97,7 +108,10 @@ class RedisService(AsyncCacheAbstract):
         return PersonFull.parse_raw(data)
 
     async def _get_list_of_objects(
-        self, page: int, size: int, query: str = None
+        self,
+        page: int,
+        size: int,
+        query: str = None
     ) -> tuple[int, list[PersonFull]]:
         """Get person list data from Redis cache."""
 
@@ -106,6 +120,7 @@ class RedisService(AsyncCacheAbstract):
 
         if not data:
             return 0, []
+
         persons_data = json.loads(data)
         persons = [
             PersonFull.parse_raw(person) for person in persons_data['persons']
@@ -126,13 +141,16 @@ class RedisService(AsyncCacheAbstract):
         )
 
     async def _put_list_of_objects(
-        self, page: int, size: int, total: int,
-        persons: list[PersonFull], query: str = None,
+        self,
+        page: int,
+        size: int,
+        total: int,
+        persons: list[PersonFull],
+        query: str = None,
     ) -> None:
         """Put person list data into Redis cache."""
 
         cache_key = f'persons:{page}:{size}:{query}'
-
         data = {
             'total': total,
             'persons': [person.json() for person in persons]
@@ -146,7 +164,8 @@ class RedisService(AsyncCacheAbstract):
         )
 
     async def _person_films_from_cache(
-        self, person_id: str
+        self,
+        person_id: str
     ) -> tuple[int, list[PersonShortFilmInfo]]:
         """Get person film list data from Redis cache."""
 
@@ -166,17 +185,20 @@ class RedisService(AsyncCacheAbstract):
         return total, films
 
     async def _put_person_films_to_cache(
-        self, person_id: str, total: int, films: list[PersonShortFilmInfo]
+        self,
+        person_id: str,
+        total: int,
+        films: list[PersonShortFilmInfo]
     ) -> None:
         """Put person film list data into Redis cache."""
 
         cache_key = f'person_films:{person_id}'
-
         data = {
             'total': total,
             'person_films': [film.json() for film in films]
         }
         json_str = json.dumps(data)
+
         await self.redis.set(
             cache_key,
             json_str,
@@ -184,17 +206,19 @@ class RedisService(AsyncCacheAbstract):
         )
 
 
-es_service = ElasticService(elastic, INDEX_NAME)
-redis_service = RedisService(redis)
+search_service = ElasticService(elastic, INDEX_NAME)
+cache_service = RedisService(redis)
 
 
 class PersonService:
-    def __init__(
-        self, elastic: AsyncElasticsearch,
-        redis: Redis, index_name: str
-    ):
-        """PersonService class initializing."""
+    """Class to represent persons logic."""
 
+    def __init__(
+        self,
+        elastic: AsyncElasticsearch,
+        redis: Redis,
+        index_name: str
+    ) -> None:
         self.elastic = elastic
         self.redis = redis
         self.index_name = index_name
@@ -202,33 +226,34 @@ class PersonService:
     async def get_by_id(self, person_id: str) -> PersonFull | None:
         """Returns data about the person by his id."""
 
-        person = await redis_service._get_single_object(person_id)
+        person = await cache_service._get_single_object(person_id)
 
         if not person:
-            person = await es_service._get_single_object(person_id)
+            person = await search_service._get_single_object(person_id)
 
             if not person:
                 return None
 
-            await redis_service._put_single_object(person)
+            await cache_service._put_single_object(person)
 
         return person
 
     async def get_person_list(
-        self, page: int, page_size: int, search_query: str | None
+        self,
+        page: int,
+        page_size: int,
+        search_query: str | None
     ) -> tuple[int, list[PersonFull]]:
         """Returns a list of person data with filtering and sorting."""
 
-        total, data = await redis_service._get_list_of_objects(
+        total, data = await cache_service._get_list_of_objects(
             page, page_size, search_query
         )
 
         if not data:
             if search_query:
                 query = {
-                    "match_phrase_prefix": {
-                        "full_name": search_query
-                    }
+                    "match_phrase_prefix": {"full_name": search_query}
                 }
             else:
                 query = {"match_all": {}}
@@ -236,24 +261,27 @@ class PersonService:
             from_page = (page - 1) * page_size
 
             try:
-                total, data = await es_service._get_list_of_objects(
+                total, data = await search_service._get_list_of_objects(
                     query=query, page_size=page_size, from_page=from_page
                 )
             except Exception as exc:
                 logging.exception('An error occured: %s', exc)
 
-            await redis_service._put_list_of_objects(
+            await cache_service._put_list_of_objects(
                 page, page_size, total, data, search_query
             )
+
             return total, data
+
         return total, data
 
     async def get_person_films_list(
-        self, person_id: str
+        self,
+        person_id: str
     ) -> tuple[int, list[PersonShortFilmInfo]]:
         """Data about films in which the person took part."""
 
-        total, films_data = await redis_service._person_films_from_cache(
+        total, films_data = await cache_service._person_films_from_cache(
             person_id
         )
 
@@ -275,11 +303,10 @@ class PersonService:
                 )
                 films_data.append(obj)
 
-            # total = len(films_data)
-
-            await redis_service._put_person_films_to_cache(
+            await cache_service._put_person_films_to_cache(
                 person_id, total, films_data
             )
+
         return total, films_data
 
 
