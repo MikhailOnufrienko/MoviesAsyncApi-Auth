eb879c8daff16650205029d31c09662e4176429f
MikhailOnufrienko <ghast13@yandex.ru>
2023-08-02 18:29:42 +0400

    Add conftest.

--- auth/src/tests/functional/config.py
+++ auth/src/tests/functional/config.py
@@ -7,14 +7,14 @@ load_dotenv()
 
 class TestSettings(BaseSettings):
     SERVICE_NAME: str = 'auth'
-    DB_HOST: str = '127.0.0.1'
-    DB_PORT: int = 5433
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_NAME: str = 'auth_test'
+    TEST_DB_HOST: str = '127.0.0.1'
+    TEST_DB_PORT: int = 5433
+    TEST_DB_USER: str
+    TEST_DB_PASSWORD: str
+    TEST_DB_NAME: str = 'auth_test'
     
     class Config:
-        env_file = '../.env'
+        env_file = '.env'
         env_file_encoding = 'utf-8'
 
 
